ccb73fcc0614aea73310f08aa2e250de1de0216f
AbdunazarovU <79502865+AbdunazarovU@users.noreply.github.com>
2023-02-15 11:22:59 +0500

    sixthSection

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../redux/actions';
 
 function Counter({ counter, inc, dec, random }) {
   return (
     <div>
       <h1>Counter: {counter}</h1>
-      <button onClick={inc} class="btn btn-primary">
+      <button onClick={inc} className="btn btn-primary">
         increment +
       </button>
-      <button onClick={dec} class="btn btn-danger">
+      <button onClick={dec} className="btn btn-danger">
         decrement -
       </button>
-      <button onClick={random} class="btn btn-info">
+      <button onClick={random} className="btn btn-info">
         random #
       </button>
     </div>
   );
 }
-export default Counter;
+
+const mapStateToProps = (state) => {
+  return {
+    counter: state.value,
+  };
+};
+
+export default connect(mapStateToProps, actions)(Counter);
--- src/redux/action.js
+++ src/redux/action.js
@@ -1,3 +1,6 @@
 export const inc = () => ({ type: 'INCREMENT' });
 export const dec = () => ({ type: 'DECREMENT' });
-export const random = (number) => ({ type: 'RANDOM', payload: number });
+export const random = (number) => ({
+  type: 'RANDOM',
+  payload: Math.floor(Math.random() * 10),
+});
